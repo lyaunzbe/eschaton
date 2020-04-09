d583ad0928439c3f488b7342e60dcb6c3e500b69
Ben Lyaunzon <benlyaunzon@Ben-Lyaunzon-1969.local>
2020-04-09 00:19:51 -0700

    first commit

---
+++ precache-manifest.40c8066677a3e0f6ee45f73c0c4da99d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126a860360193f48828a1029ab26ef79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b72e86ac92b0b0fb9357",
+    "url": "/static/css/main.e66736a5.chunk.css"
+  },
+  {
+    "revision": "5f306c5353a37fb903e6",
+    "url": "/static/js/2.332f4a83.chunk.js"
+  },
+  {
+    "revision": "300a0a01a49a6ce568c9b658dbd7888d",
+    "url": "/static/js/2.332f4a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72e86ac92b0b0fb9357",
+    "url": "/static/js/main.ee47c87d.chunk.js"
+  },
+  {
+    "revision": "9f0dcbe0951bcc954454",
+    "url": "/static/js/runtime-main.39aee35a.js"
+  },
+  {
+    "revision": "7942922aeb25f84924570aa420bc11b2",
+    "url": "/static/media/disp1.7942922a.jpg"
+  },
+  {
+    "revision": "7951324490d2d2689429966f32450f4d",
+    "url": "/static/media/img11.79513244.jpg"
+  },
+  {
+    "revision": "f40d7b8e3905ba21e79830074537b09a",
+    "url": "/static/media/img12.f40d7b8e.jpg"
+  }
+]);
